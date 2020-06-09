0d0ae106ab3c391732e2889bd951d7deebce7d1b
“nahuel” <n.gorosito@yahoo.com>
2020-06-09 12:59:23 -0400

    added 5 day calendar span to my splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
-// import { Route } from 'react-router-dom';
+// import SplashCalendar from './splash_calendar';
 
 class SplashPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    }
+  }
+
+
+
   render() {
+    let yesterday = new Date(this.state.date);
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    let yesterdayMinus = new Date(yesterday);
+    yesterdayMinus.setDate(yesterdayMinus.getDate() - 1);
+
+    let tomorrow = new Date(this.state.date);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
+    let tomorrowPlus = new Date(tomorrow);
+    tomorrowPlus.setDate(tomorrowPlus.getDate() + 1);
+
+
+    let weekday = this.state.date.toDateString().split(' ')[0];
+    let today = this.state.date.getDate();
     return (
      <div>
        <div className="splash-header-container">
@@ -17,7 +41,25 @@ class SplashPage extends React.Component {
        </div>
        <div className="splash-content-container">
           <nav className="main-banner">
-            <h1>Denizen Confidant</h1>
+            <h1 className="denizen-confidant">Denizen Confidant</h1>
+            {/* <SplashCalendar /> */}
+            <ul className="days">
+              <li className="day">{yesterdayMinus.toDateString().split(' ')[0]}
+                <h1>{yesterdayMinus.getDate()}</h1>
+              </li>
+              <li className="day">{yesterday.toDateString().split(' ')[0]}
+                <h1>{yesterday.getDate()}</h1>
+              </li>
+              <li className="day">{weekday}  
+                <h1>{today}</h1>
+              </li>
+              <li className="day">{tomorrow.toDateString().split(' ')[0]}
+                <h1>{tomorrow.getDate()}</h1>
+              </li>
+              <li className="day">{tomorrowPlus.toDateString().split(' ')[0]}
+                <h1>{tomorrowPlus.getDate()}</h1>
+              </li>
+            </ul>
           </nav> 
        </div>
      </div>
