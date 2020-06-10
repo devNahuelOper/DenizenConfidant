8c96b1d12e7cbbecefa6a17f07fca32e481a4f0f
“nahuel” <n.gorosito@yahoo.com>
2020-06-10 15:36:11 -0400

    fixed year dropdown overflow, adjusted hovering on all dropdowns so parent and children hover independently

--- frontend/components/greeting/dropdown.jsx
+++ frontend/components/greeting/dropdown.jsx
@@ -25,8 +25,8 @@ class Dropdown extends React.Component {
      <div>
        <button onFocus={this.clicker} onBlur={this.leave} className="login-signup">Login / Register
          <ul className={this.state.drop ? "reveal" : "hide"}>
-            <li><Link className="log-link" to="/login">Login</Link></li>
-            <li><Link className="log-link" to="/signup">Register</Link></li>
+            <li><Link onClick={this.leave} className="log-link" to="/login">Login</Link></li>
+            <li><Link onClick={this.leave} className="log-link" to="/signup">Register</Link></li>
          </ul>
        </button> 
      </div>
