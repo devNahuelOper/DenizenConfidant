002611565fea46a19afd47fb4a1d9f9aac571630
“nahuel” <n.gorosito@yahoo.com>
2020-07-28 13:10:35 -0400

    setup redux cycle for genres

---
+++ frontend/actions/genre_actions.js
@@ -0,0 +1,19 @@
+import * as GenreApiUtil from '../util/genre_api_util';
+
+export const RECEIVE_GENRES = 'RECEIVE_GENRES';
+export const RECEIVE_GENRE = 'RECEIVE_GENRE';
+
+export const receiveGenres = genres => ({
+  type: RECEIVE_GENRES,
+  genres
+})
+
+export const receiveGenre = genre => ({
+  type: RECEIVE_GENRE,
+  genre
+});
+
+export const fetchGenres = genres => dispatch => (
+  GenreApiUtil.fetchGenres(genres)
+    .then(genres => dispatch(receiveGenres(genres)))
+)
