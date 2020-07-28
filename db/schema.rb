1acffe264c3557e2f6dae8252af628c69862300d
“nahuel” <n.gorosito@yahoo.com>
2020-07-28 12:07:12 -0400

    create genre model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_215810) do
+ActiveRecord::Schema.define(version: 2020_07_28_155025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(version: 2020_07_15_215810) do
     t.string "location", null: false
     t.date "date"
     t.text "headliners"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.string "origin"
+    t.text "artists", default: [], array: true
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
