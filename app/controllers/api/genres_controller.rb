72197a4fdca2ae4e3e8a5c99200a3e3feb9149c1
“nahuel” <n.gorosito@yahoo.com>
2020-07-28 12:33:59 -0400

    create genre controller

---
+++ app/controllers/api/genres_controller.rb
@@ -0,0 +1,18 @@
+class Api::GenresController < ApplicationController
+
+  def index
+    @genres = Genre.all
+    render :index
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+    render :show
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name, :origin, :artists, :description, :photo)
+  end
+end
