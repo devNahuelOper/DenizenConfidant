bd1cec09727373050df00f15bcc7000cb2b443dc
“nahuel” <n.gorosito@yahoo.com>
2020-07-29 19:49:41 -0400

    links to genre show pages from dj show pages

--- frontend/components/djs/dj_show_container.js
+++ frontend/components/djs/dj_show_container.js
@@ -1,18 +1,21 @@
 import { connect } from 'react-redux';
 import DjShow from './dj_show';
 import { fetchDj } from '../../actions/dj_actions';
+import { fetchGenres } from '../../actions/genre_actions';
 
 
 
 const mapStateToProps = (state, ownProps) => {
   return {
     dj: state.entities.djs[ownProps.match.params.djId],
-    name: state.entities.djs[ownProps.match.params.djId].name
+    name: state.entities.djs[ownProps.match.params.djId].name,
+    genres: Object.values(state.entities.genres)
   }
 };
 
 const mapDispatchToProps = dispatch => ({
-  fetchDj: djId => dispatch(fetchDj(djId))
+  fetchDj: djId => dispatch(fetchDj(djId)),
+  fetchGenres: () => dispatch(fetchGenres())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(DjShow);
