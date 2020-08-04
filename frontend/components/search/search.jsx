4b3205c455a73f30e374e1692565f3352c8486b1
“nahuel” <n.gorosito@yahoo.com>
2020-08-04 11:28:29 -0400

    add events to search functionality

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchDjs } from '../../actions/dj_actions';
 import { fetchGenres } from '../../actions/genre_actions';
+import { fetchEvents } from '../../actions/event_actions';
 
 
 
@@ -12,23 +13,26 @@ class Search extends React.Component {
     this.state = {
       djs: [],
       genres: [],
+      events: [],
       searchTerm: ''
     }
     this.editSearchTerm = this.editSearchTerm.bind(this);
-    this.dynamicSearch = this.dynamicSearch.bind(this);
+    this.djSearch = this.djSearch.bind(this);
     this.genreSearch = this.genreSearch.bind(this);
+    this.eventSearch = this.eventSearch.bind(this);
   }
 
   componentDidMount() {
     this.props.fetchDjs().then(djs => this.setState({ djs: Object.values(djs.djs) }));
     this.props.fetchGenres().then(genres => this.setState({ genres: Object.values(genres.genres)}));    
+    this.props.fetchEvents().then(events => this.setState({ events: Object.values(events.events)}));    
   }
 
   editSearchTerm(e) {
     this.setState({searchTerm: e.target.value})
   }
 
-  dynamicSearch(e) {
+  djSearch() {
     return this.state.djs.filter(dj => dj.name.toLowerCase().startsWith(this.state.searchTerm.toLowerCase())); 
   }
   
@@ -36,15 +40,19 @@ class Search extends React.Component {
     return this.state.genres.filter(genre => genre.name.toLowerCase().startsWith(this.state.searchTerm.toLowerCase()));
   }
 
+  eventSearch() {
+    return this.state.events.filter(event => event.name.toLowerCase().startsWith(this.state.searchTerm.toLowerCase()));
+  }
+
   render() {
     return (
       <div className="search-container">
         <div id="searchbar" >
-        <form onSubmit={this.dynamicSearch}>
+        <form id="search-form" onSubmit={this.djSearch}>
           <input type="text" id="search-input" 
           value={this.state.searchTerm}
           onChange={this.editSearchTerm}
-          placeholder="DJs, Genres"
+          placeholder="DJs, Genres, Events"
           />
           <div id="search-button-container">
             <button id="search-button">Submit</button>
@@ -53,8 +61,9 @@ class Search extends React.Component {
             style={this.state.searchTerm.length ? { display: 'block' } : { display: 'none' }}
           >
           <ul id="searchlist">
-            <li><DjQueryContainer djs = {this.dynamicSearch()}/></li>
+            <li><DjQueryContainer djs = {this.djSearch()}/></li>
             <li><GenreQueryContainer genres = {this.genreSearch()}/></li>
+            <li><EventQueryContainer events = {this.eventSearch()}/></li>
           </ul>
           </div>
           </form>
@@ -85,11 +94,21 @@ class GenreQueryContainer extends React.Component {
   }
 }
 
+class EventQueryContainer extends React.Component {
+  render() {
+    return (
+      <div id="event-query-container">
+        {this.props.events.map(event => <EventQuery event={event} />)}
+      </div>
+    )
+  }
+}
+
 class DjQuery extends React.Component {
   render() {
     return (
       <Link to={`/djs/${this.props.dj.id}`}>
-      <div className="query-item" id="dj-query-item">
+        <div className="query-item" id="dj-query-item">
         <strong>{this.props.dj.name}</strong> <small>DJ</small>
       </div>
       </Link >
@@ -109,16 +128,30 @@ class GenreQuery extends React.Component {
   }
 }
 
+class EventQuery extends React.Component {
+  render() {
+    return (
+      <Link to={`/events/${this.props.event.id}`}>
+        <div className="query-item" id="event-query-item">
+          <strong>{this.props.event.name}</strong> <small>Event</small>
+        </div>
+      </Link >
+    )
+  }
+}
+
 const mapStateToProps = (state) => {
   return {
     djs: Object.values(state.entities.djs),
-    genres: Object.values(state.entities.genres)
+    genres: Object.values(state.entities.genres),
+    events: Object.values(state.entities.events)
   }
 };
 
 const mapDispatchToProps = dispatch => ({
   fetchDjs: () => dispatch(fetchDjs()),
-  fetchGenres: () => dispatch(fetchGenres())
+  fetchGenres: () => dispatch(fetchGenres()),
+  fetchEvents: () => dispatch(fetchEvents())
 });
 
 
