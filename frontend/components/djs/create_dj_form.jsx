a910fff089d4b317d1b915ea6b47b85dbf597e54
“nahuel” <n.gorosito@yahoo.com>
2020-08-14 09:13:07 -0400

    setup create dj form

--- frontend/components/djs/create_dj_form.jsx
+++ frontend/components/djs/create_dj_form.jsx
@@ -24,9 +24,52 @@ class CreateDjForm extends React.Component {
             <h1>DJs</h1>
           </section>
         </div>
+        <div className="djs-subnav-container">
+          <section className="djs-subnav">
+            <ul>
+              <li><Link className="form" to="/djs">All</Link></li>
+              <li><Link to="/">Take me back home</Link></li>
+              <li id="pending"><Link to='/djs'>Create an artist profile <br /> <small>Coming Soon!</small></Link></li>
+            </ul>
+          </section>
+          <SubnavToggle />
+        </div>
       </div>
     )
   }
 };
+
+class SubnavToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drop: false
+    }
+    this.clicker = this.clicker.bind(this);
+    this.leave = this.leave.bind(this);
+  }
+
+  clicker(e) {
+    this.setState({ "drop": true });
+  }
+
+  leave(e) {
+    this.setState({ "drop": false });
+  }
+
+  render() {
+    return (
+      <div className="subnav-toggle" id={this.state.drop ? "expand" : "normal"}>
+        <button className="subnav-drop" onFocus={this.clicker} onTap={this.clicker} onBlur={this.leave}> <span>All <small>⬇︎</small></span>
+          <ul className={this.state.drop ? "reveal" : "hide"}>
+            <li><Link className="log-link" onClick={this.leave} to="/">Take me back home</Link></li>
+            <li><Link className="log-link" onClick={this.leave} to="/signup">Create and artist profile</Link></li>
+          </ul>
+        </button>
+      </div>
+    )
+  }
+}
+
 
 export default withRouter(CreateDjForm);
