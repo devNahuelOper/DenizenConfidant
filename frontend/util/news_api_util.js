a7c30b367504c684f17ce8d5c2c88b1cf70b29c1
“nahuel” <n.gorosito@yahoo.com>
2020-08-25 10:32:55 -0400

    setup news

--- frontend/util/news_api_util.js
+++ frontend/util/news_api_util.js
@@ -2,11 +2,23 @@
 
 // const content = weBrowser.Document.getElementsByClassName('content-list')[0];
 
-export const fetchAllNews = news => (
+// export const fetchAllNews = news => (
+//   $.ajax({
+//     url: 'https://cors-anywhere.herokuapp.com/https://www.residentadvisor.net/news',
+//     data: news,
+//     dataType: 'html',
+//     success: 'success'
+//   })
+// )
+
+export const fetchAllNews = () => (
   $.ajax({
-    url: 'https://cors-anywhere.herokuapp.com/https://www.residentadvisor.net/news',
-    data: news,
-    dataType: 'html',
-    success: 'success'
+    url: 'api/news'
   })
-)
+);
+
+export const fetchNews = newsId => (
+  $.ajax({
+    url: `api/news/${newsId}`
+  })
+);
