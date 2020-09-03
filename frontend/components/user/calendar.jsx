314f394bb18e081d15e77ef72e5a119e38209dd0
“nahuel” <n.gorosito@yahoo.com>
2020-09-03 08:44:51 -0400

    setup calendar

--- frontend/components/user/calendar.jsx
+++ frontend/components/user/calendar.jsx
@@ -7,10 +7,12 @@ const Calendar = () => {
   let dayOne = firstDay();
   let week = Array.from(Array(8).keys()).slice(1);
   let month = Array.from(Array(6)).slice(1).fill(week);
-  let $exactMonth = $('td').slice(dayOne, numDays + 2);
+  // let $exactMonth = $('td').slice(dayOne, numDays + 2);
+  $('td').slice(dayOne, numDays + 2).each(function(idx) {
+    $(this).text(idx+1);
+  })
   return (
     <table className="calendar">
-      <h1>{dayOne}</h1>
       <tbody>
         <tr className="weekdays">
           <th id="sun">Sun</th>
