cdf958a7b58fe6d78f9d87e47bb6fa395012416e
“nahuel” <n.gorosito@yahoo.com>
2020-09-08 12:36:22 -0400

    adjust media queries for mobile scale

--- frontend/components/genres/genre_index_item.jsx
+++ frontend/components/genres/genre_index_item.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const GenreIndexItem = ({ genre }) => (
   <li className="genrelist-item" key={genre.id}>
     <article className="genre">
-      <Link id="genre-name" to={`genres/${genre.id}`}>
+      <Link className="genre-name" to={`genres/${genre.id}`}>
       <img className="genre-avatar" src={genre.photoUrl} alt={genre.name}/> &nbsp;
-       <strong>{genre.name}</strong>
+       <strong id={genre.name.split('-')[0].toLowerCase()}>{genre.name}</strong>
         </Link>
     </article>
   </li>
