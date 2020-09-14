86fa1fa0ddb97493b89d6683bd49114762f09390
“nahuel” <n.gorosito@yahoo.com>
2020-09-14 12:00:48 -0400

    add title to calendar

--- frontend/components/user/calendar.jsx
+++ frontend/components/user/calendar.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { daysInMonth, firstDay, daysInLastMonth } from '../../util/date_util';
+import { daysInMonth, firstDay, daysInLastMonth, formatMonthYear } from '../../util/date_util';
 
 
 const Calendar = () => {
+  let now = new Date();
+  let dispYear = formatMonthYear(now).split(' ')[1];
+  let dispMonth = formatMonthYear(now).split(' ')[0];
 
   const week = Array.from(Array(8).keys()).slice(1);
   const month = Array.from(Array(6)).slice(1).fill(week);
@@ -25,29 +28,39 @@ const Calendar = () => {
     $(this).parent().css({ color: '#787878', background: '#3c3c3c' });
   })
   return (
-    <table className="calendar">
-      <tbody>
-        <tr className="weekdays">
-          <th id="sun">Sun</th>
-          <th id="mon">Mon</th>
-          <th id="tue">Tue</th>
-          <th id="wed">Wed</th>
-          <th id="thu">Thu</th>
-          <th id="fri">Fri</th>
-          <th id="sat">Sat</th>
-        </tr>
-        {month.map((week, i) =>
-        <tr className="week" key={`week-${i}`}>
-          {week.map(day => 
-          <td className="cal-day" key={day}>
-            <b></b>
-            <hr/>
-            </td>
+    <div className="calendar">
+      <ul className="year-month">
+        <li>
+          <span>{dispYear}</span>
+        </li>
+        <li>
+          <span>{dispMonth}</span>
+        </li>
+      </ul>
+      <table className="calendar">
+        <tbody>
+          <tr className="weekdays">
+            <th id="sun">Sun</th>
+            <th id="mon">Mon</th>
+            <th id="tue">Tue</th>
+            <th id="wed">Wed</th>
+            <th id="thu">Thu</th>
+            <th id="fri">Fri</th>
+            <th id="sat">Sat</th>
+          </tr>
+          {month.map((week, i) =>
+          <tr className="week" key={`week-${i}`}>
+            {week.map(day => 
+            <td className="cal-day" key={day}>
+              <b></b>
+              <hr/>
+              </td>
+              )}
+          </tr>
             )}
-        </tr>
-          )}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
