e580a9e87b131e86bc6493a541d57e7721726e91
“nahuel” <n.gorosito@yahoo.com>
2020-09-16 12:01:21 -0400

    setup search page

---
+++ frontend/components/search/search_page.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class SearchPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <TitleComponent title='DC: Search' />
+        <div className="search-page">
+          <header>
+            <div id="nav-container">
+              <section id="navbar">
+                <nav>
+                  <Link to="/"><img src={window.logoUrl} id="logo" /></Link>
+                  <ul id="links">
+                    <li><Link to="/djs">DJs</Link></li>
+                    <li><Link to="/events">Events</Link></li>
+                    <li><Link to="/genres">Music</Link></li>
+                    <li><button id="search">Search</button></li>
+                  </ul>
+                </nav>
+                <h1>Search</h1>
+              </section>
+            </div>
+          </header>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default SearchPage;
