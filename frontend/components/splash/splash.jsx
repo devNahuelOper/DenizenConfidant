3ca1d24e6609067a9bbfe372509fc9d2fba9501b
“nahuel” <n.gorosito@yahoo.com>
2020-09-21 10:09:29 -0400

    remove dead code

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -104,8 +104,6 @@ class SplashPage extends React.Component {
                   <article>
                     <img src={window.gesaffelsteinUrl} id="gesaffelstein"/>
                     <Link id="gesaffelstein-link" to="/djs/739" title="Gesaffelstein">Gesaffelstein - Depravity</Link>
-                    {/* <Link id="gesaffelstein-link" to={`/djs/${gesaffId}`}>Gesaffelstein - Depravity</Link> */}
-                    {/* <Link to={`/djs/${djs.find(dj => dj.name === 'Gesaffelstein')}`}>Gesaffelstein - Depravity</Link> */}
                     <audio id="depravity" src="depravity.mp3" controls></audio>
                   </article>
                 </li>
