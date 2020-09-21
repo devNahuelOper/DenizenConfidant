26627432217ddeeb5de56d975cf12ababe63d5da
“nahuel” <n.gorosito@yahoo.com>
2020-09-21 11:21:19 -0400

    dry up code with navbar component

--- frontend/components/user/user_profile.jsx
+++ frontend/components/user/user_profile.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchCurrentUser, getCurrentUser } from '../../actions/session_actions';
 import { Link } from 'react-router-dom';
+import NavBar from '../navbar/navbar';
 import {
   formatMonthYear,
   formatLastOnline,
@@ -67,18 +68,7 @@ class UserProfile extends React.Component {
       <div className="user-profile">
         <header>
           <div id="nav-container">
-            <section id="navbar">
-              <nav>
-                <Link to="/"><img src={window.logoUrl} id="logo" /></Link>
-                <ul id="links">
-                  <li><Link to="/djs">DJs</Link></li>
-                  <li><Link to="/events">Events</Link></li>
-                  <li><Link to="/genres">Music</Link></li>
-                  <li><button id="search">Search</button></li>
-                </ul>
-              </nav>
-              <h1>{currentUser.username}</h1>
-            </section>
+            <NavBar title={currentUser.username}/>
           </div>
         </header>
         <div className="subnav-container">
