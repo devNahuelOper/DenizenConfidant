6cefff7d97f7d5c64abc391cc19371ac724de7f2
“nahuel” <n.gorosito@yahoo.com>
2020-09-23 08:04:19 -0400

    dry up components with subnav toggle component

--- frontend/components/user/user_profile.jsx
+++ frontend/components/user/user_profile.jsx
@@ -117,58 +117,6 @@ class UserProfile extends React.Component {
   }
 }
 
-// class SubnavToggle extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       drop: false
-//     }
-//     this.clicker = this.clicker.bind(this);
-//     this.leave = this.leave.bind(this);
-//     this.toggleCalendar = this.toggleCalendar.bind(this);
-//     this.mobileDrop = this.mobileDrop.bind(this);
-//   }
-
-//   clicker(e) {
-//     this.setState({ "drop": true });
-//   }
-
-//   leave(e) {
-//     this.setState({ "drop": false });
-//   }
-
-  // toggleCalendar() {
-  //   this.props.toggleCalendar();
-  //   this.leave();
-  // }
-
-//   mobileDrop(e) {
-//     e.preventDefault();
-//     this.setState({ "drop": true }); status
-//     $('body').on('click', e => {
-//       const drop = $('.subnav-drop');
-//       if (drop !== e.currentTarget) {
-//         this.leave();
-//       }
-//     })
-//   }
-
-//   render() {
-//     const { currentUser } = this.props;
-//     return (
-//       <div className="subnav-toggle" id={this.state.drop ? "expand" : "normal"}>
-//         <button className="subnav-drop" onFocus={this.clicker} onBlur={this.leave}> <span onClick={this.mobileDrop} id="overview">Overview <small>⬇︎</small></span>
-//           <ul className={this.state.drop ? "reveal" : "hide"}>
-//             <li id="calendar-toggle" onClick={this.toggleCalendar}><Link className="log-link" to={`/users/${currentUser.id}`}>Calendar</Link></li>
-//             <li><Link className="log-link" to={`/users/${currentUser.id}/events`}>My Events</Link></li>
-//             {/* <li id="user-reveal"><Link className="log-link" onClick={this.leave} to="/">Take me back home</Link></li> */}
-//           </ul>
-//         </button>
-//       </div>
-//     )
-//   }
-// }
-// onBlur = { this.leave }
 
 const mapStateToProps = (state) => {
   return {
