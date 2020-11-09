184bc9a29d395849685d874b4869cb014382a534
“nahuel” <n.gorosito@yahoo.com>
2020-11-09 14:23:48 -0500

    prettify update event form

--- frontend/components/events/update_event_form_container.jsx
+++ frontend/components/events/update_event_form_container.jsx
@@ -1,18 +1,26 @@
-import { connect } from 'react-redux';
-import { fetchEvent, updateEvent, receiveEventErrors, clearEventErrors } from '../../actions/event_actions';
-import { fetchCurrentUser, getCurrentUser } from '../../actions/session_actions';
-import UpdateEventForm from './update_event_form';
+import { connect } from "react-redux";
+import {
+  fetchEvent,
+  updateEvent,
+  receiveEventErrors,
+  clearEventErrors,
+} from "../../actions/event_actions";
+import {
+  fetchCurrentUser,
+  getCurrentUser,
+} from "../../actions/session_actions";
+import UpdateEventForm from "./update_event_form";
 
 const mapStateToProps = (state, ownProps) => {
   return {
     currentUser: getCurrentUser(state),
     event: state.entities.events[ownProps.match.params.eventId],
-  }
+  };
   // errors: state.errors.events
 };
 
-const mapDispatchToProps = dispatch => ({
-  fetchEvent: eventId => dispatch(fetchEvent(eventId)),
+const mapDispatchToProps = (dispatch) => ({
+  fetchEvent: (eventId) => dispatch(fetchEvent(eventId)),
   fetchCurrentUser: (userId) => dispatch(fetchCurrentUser(userId)),
   updateEvent: (event, id) => dispatch(updateEvent(event, id)),
   // updateEventPhoto: (event, id) => dispatch(updateEventPhoto(event, id)),
