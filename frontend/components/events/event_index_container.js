5f98c79181e7ab0d65becb18e55d269a81b53fab
“nahuel” <n.gorosito@yahoo.com>
2021-01-01 09:32:02 -0500

    prettify dj index container

--- frontend/components/events/event_index_container.js
+++ frontend/components/events/event_index_container.js
@@ -1,16 +1,16 @@
-import { connect } from 'react-redux';
-import EventIndex from './event_index';
-import { fetchEvents, deleteEvent } from '../../actions/event_actions';
+import { connect } from "react-redux";
+import EventIndex from "./event_index";
+import { fetchEvents, deleteEvent } from "../../actions/event_actions";
 
 const mapStateToProps = (state) => {
   return {
-    events: Object.values(state.entities.events)
-  }
+    events: Object.values(state.entities.events),
+  };
 };
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   fetchEvents: () => dispatch(fetchEvents()),
-  deleteEvent: eventId => dispatch(deleteEvent(eventId))
+  deleteEvent: (eventId) => dispatch(deleteEvent(eventId)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventIndex);
