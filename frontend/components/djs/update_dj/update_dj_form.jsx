ca1c0d3a7c8bb38770d66ca48e571da5534762a2
“nahuel” <n.gorosito@yahoo.com>
2021-01-28 12:46:20 -0500

    Setup update dj form container

---
+++ frontend/components/djs/update_dj/update_dj_form.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { TitleComponent } from "../title_component.jsx";
+import SubnavToggle from "../subnav/subnav";
+
+
+class UpdateDjForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = this.props.dj;
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    this.props.fetchDj(this.props.match.params.djId);
+  }
+
+  render() {
+    const { dj, currentUser } = this.props;
+    if (!dj) return null;
+
+    return (
+      <React.Fragment>
+        <TitleComponent title={`DC: Update ${dj.name}`} />
+      </React.Fragment>
+    );
+  }
+}
