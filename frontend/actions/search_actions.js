448da4375f39c3ce929bff6a3be8f2476428a884
“nahuel” <n.gorosito@yahoo.com>
2021-02-10 17:19:05 -0500

    Setup entity search action creators

--- frontend/actions/search_actions.js
+++ frontend/actions/search_actions.js
@@ -2,9 +2,9 @@ export const SHOW_SEARCH = "SHOW_SEARCH";
 export const HIDE_SEARCH = "HIDE_SEARCH";
 export const SET_SEARCH_TERM = "SET_SEARCH_TERM";
 export const CLEAR_SEARCH_TERM = "CLEAR_SEARCH_TERM";
-// export const SEARCH_DJS = "SEARCH_DJS";
-// export const SEARCH_EVENTS = "SEARCH_EVENTS";
-// export const SEARCH_GENRES = "SEARCH_GENRES";
+export const SEARCH_DJS = "SEARCH_DJS";
+export const SEARCH_EVENTS = "SEARCH_EVENTS";
+export const SEARCH_GENRES = "SEARCH_GENRES";
 
 export const showSearch = (searchBar) => {
   return {
@@ -34,10 +34,23 @@ export const clearSearchTerm = (searchTerm) => {
   };
 };
 
-// export const searchDjs = (searchTerm, djs) => {
-//   return {
-//     type: SEARCH_DJS,
-//     searchTerm,
-//     djs,
-//   };
-// };
+export const searchDjs = (djs) => {
+  return {
+    type: SEARCH_DJS,
+    djs,
+  };
+};
+
+export const searchEvents = (events) => {
+  return {
+    type: SEARCH_EVENTS,
+    events,
+  };
+};
+
+export const searchGenres = (genres) => {
+  return {
+    type: SEARCH_GENRES,
+    genres,
+  };
+};
