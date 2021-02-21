2f0a71c28c7696bc7052ed357f90567a70a81b77
“nahuel” <n.gorosito@yahoo.com>
2021-02-21 12:54:58 -0500

    Remove all redundant months and days variables from date util

--- frontend/util/date_util.js
+++ frontend/util/date_util.js
@@ -1,28 +1,53 @@
-  const months = {
-    0: "January",
-    1: "February",
-    2: "March",
-    3: "April",
-    4: "May",
-    5: "June",
-    6: "July",
-    7: "August",
-    8: "September",
-    9: "October",
-    10: "November",
-    11: "December",
-  };
-  const daysOfWeek = {
-    0: "Sunday",
-    1: "Monday",
-    2: "Tuesday",
-    3: "Wednesday",
-    4: "Thursday",
-    5: "Friday",
-    6: "Saturday",
-  };
+const months = {
+  0: "January",
+  1: "February",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "September",
+  9: "October",
+  10: "November",
+  11: "December",
+};
+const shortMonths = {
+  0: "Jan",
+  1: "Feb",
+  2: "Mar",
+  3: "Apr",
+  4: "May",
+  5: "Jun",
+  6: "Jul",
+  7: "Aug",
+  8: "Sep",
+  9: "Oct",
+  10: "Nov",
+  11: "Dec",
+};
 
-export const formatDate = date => {
+const daysOfWeek = {
+  0: "Sunday",
+  1: "Monday",
+  2: "Tuesday",
+  3: "Wednesday",
+  4: "Thursday",
+  5: "Friday",
+  6: "Saturday",
+};
+
+const shortDays = {
+  0: "Sun",
+  1: "Mon",
+  2: "Tue",
+  3: "Wed",
+  4: "Thu",
+  5: "Fri",
+  6: "Sat",
+};
+
+export const formatDate = (date) => {
   const obj = new Date(date);
   obj.setMinutes(obj.getMinutes() + obj.getTimezoneOffset());
   const month = months[obj.getMonth()];
@@ -32,17 +57,16 @@ export const formatDate = date => {
   return `${month} ${day}, ${year} (${dayOfWeek})`;
 };
 
-export const formatLastOnline = date => {
+export const formatLastOnline = (date) => {
   const obj = new Date(date);
   obj.setMinutes(obj.getMinutes() + obj.getTimezoneOffset());
   const month = months[obj.getMonth()];
   const day = obj.getDate();
   const year = obj.getFullYear();
-  const dayOfWeek = daysOfWeek[obj.getDay()];
   return `${day} ${month} ${year}`;
 };
 
-export const formatAbsDate = date => {
+export const formatAbsDate = (date) => {
   const obj = new Date(date);
   obj.setMinutes(obj.getMinutes() + obj.getTimezoneOffset());
   const month = obj.getMonth();
@@ -52,136 +76,58 @@ export const formatAbsDate = date => {
   return {
     day: day,
     month: month,
-    year: year
+    year: year,
   };
 };
 
-export const formatMonthYear = date => {
+export const formatMonthYear = (date) => {
   const obj = new Date(date);
   obj.setMinutes(obj.getMinutes() + obj.getTimezoneOffset());
   const month = months[obj.getMonth()];
-  const day = obj.getDate();
   const year = obj.getFullYear();
-  const dayOfWeek = daysOfWeek[obj.getDay()];
   return `${month} ${year}`;
 };
 
-export const formatDateStyle = date => {
-  const months = {
-    0: 'Jan',
-    1: 'Feb',
-    2: 'Mar',
-    3: 'Apr',
-    4: 'May',
-    5: 'Jun',
-    6: 'Jul',
-    7: 'Aug',
-    8: 'Sep',
-    9: 'Oct',
-    10: 'Nov',
-    11: 'Dec',
-  };
-  const daysOfWeek = {
-    0: 'Sun',
-    1: 'Mon',
-    2: 'Tue',
-    3: 'Wed',
-    4: 'Thu',
-    5: 'Fri',
-    6: 'Sat',
-  };
+export const formatDateStyle = (date) => {
   const obj = new Date(date);
   obj.setMinutes(obj.getMinutes() + obj.getTimezoneOffset());
-  const month = months[obj.getMonth()];
+  const month = shortMonths[obj.getMonth()];
   const day = obj.getDate();
   const year = obj.getFullYear();
-  const dayOfWeek = daysOfWeek[obj.getDay()];
+  const dayOfWeek = shortDays[obj.getDay()];
+
   return `${dayOfWeek}, ${day} ${month} ${year}`;
-}
+};
 
-export const formatDateShowStyle = date => {
-  const months = {
-    0: 'Jan',
-    1: 'Feb',
-    2: 'Mar',
-    3: 'Apr',
-    4: 'May',
-    5: 'Jun',
-    6: 'Jul',
-    7: 'Aug',
-    8: 'Sep',
-    9: 'Oct',
-    10: 'Nov',
-    11: 'Dec',
-  };
-  const daysOfWeek = {
-    0: 'Sunday',
-    1: 'Monday',
-    2: 'Tuesday',
-    3: 'Wednesday',
-    4: 'Thursday',
-    5: 'Friday',
-    6: 'Saturday',
-  };
+export const formatDateShowStyle = (date) => {
   const obj = new Date(date);
   obj.setMinutes(obj.getMinutes() + obj.getTimezoneOffset());
-  const month = months[obj.getMonth()];
+  const month = shortMonths[obj.getMonth()];
   const day = obj.getDate();
   const year = obj.getFullYear();
   const dayOfWeek = daysOfWeek[obj.getDay()];
+
   return `${dayOfWeek} ${day} ${month} ${year}`;
-}
+};
 
-export const formatDateNews = date => {
-  const daysOfWeek = {
-    0: 'Sun',
-    1: 'Mon',
-    2: 'Tue',
-    3: 'Wed',
-    4: 'Thu',
-    5: 'Fri',
-    6: 'Sat',
-  };
+export const formatDateNews = (date) => {
   const obj = new Date(date);
   obj.setMinutes(obj.getMinutes() + obj.getTimezoneOffset());
-  const dayOfWeek = daysOfWeek[obj.getDay()];
+  const dayOfWeek = shortDays[obj.getDay()];
   return `${dayOfWeek}, ${formatTime(date)}`;
-}
+};
 
-export const formatDateNewsShow = date => {
-  const months = {
-    0: 'Jan',
-    1: 'Feb',
-    2: 'Mar',
-    3: 'Apr',
-    4: 'May',
-    5: 'Jun',
-    6: 'Jul',
-    7: 'Aug',
-    8: 'Sep',
-    9: 'Oct',
-    10: 'Nov',
-    11: 'Dec',
-  };
-  const daysOfWeek = {
-    0: 'Sun',
-    1: 'Mon',
-    2: 'Tue',
-    3: 'Wed',
-    4: 'Thu',
-    5: 'Fri',
-    6: 'Sat',
-  };
+export const formatDateNewsShow = (date) => {
   const obj = new Date(date);
   obj.setMinutes(obj.getMinutes() + obj.getTimezoneOffset());
-  const month = months[obj.getMonth()];
+  const month = shortMonths[obj.getMonth()];
   const day = obj.getDate();
   const year = obj.getFullYear();
-  const dayOfWeek = daysOfWeek[obj.getDay()];
+  const dayOfWeek = shortDays[obj.getDay()];
   return `${dayOfWeek}/ ${day}~${month} ${year}~/ ${formatTime(date)}`;
-}
+};
 
-export const formatTime = date => {
+export const formatTime = (date) => {
   const obj = new Date(date);
   const fullHours = obj.getHours();
   let hours = fullHours % 12;
@@ -189,13 +135,12 @@ export const formatTime = date => {
   const minutes = obj.getMinutes();
   const tmp = `0${minutes}`;
   const paddedMinutes = tmp.slice(tmp.length - 2);
-  const ampm = fullHours < 12 || fullHours === 0 ? 'am' : 'pm';
+  const ampm = fullHours < 12 || fullHours === 0 ? "am" : "pm";
   return `${hours}:${paddedMinutes}${ampm}`;
 };
 
-export const formatDateTime = date => (
-  `${formatDate(date)} ${formatTime(date)}`
-);
+export const formatDateTime = (date) =>
+  `${formatDate(date)} ${formatTime(date)}`;
 
 export const daysInMonth = () => {
   let now = new Date();
@@ -210,7 +155,7 @@ export const daysInLastMonth = () => {
 export const firstDay = () => {
   let now = new Date();
   return new Date(now.getFullYear(), now.getMonth(), 1).getDay();
-}
+};
 
 export const fiveDaySpan = (date) => {
   const dates = [];
@@ -220,8 +165,11 @@ export const fiveDaySpan = (date) => {
     dates.push(newDate);
   }
 
-  return dates.map(date => [date.toDateString().split(' ')[0], date.getDate()]);
-}
+  return dates.map((date) => [
+    date.toDateString().split(" ")[0],
+    date.getDate(),
+  ]);
+};
 
 const wordNums = {
   13: "Thirteen",
@@ -231,8 +179,8 @@ const wordNums = {
   17: "Seventeen",
   18: "Eighteen",
   19: "Nineteen",
-  20: "Twenty"
-}
+  20: "Twenty",
+};
 
 export const timeSinceBeganCoding = () => {
   const start = new Date(2019, 11, 21);
@@ -241,5 +189,4 @@ export const timeSinceBeganCoding = () => {
   let hours = seconds / 60 / 60;
   let months = Math.round(hours / 24 / 30);
   return months <= 20 ? wordNums[months] : months;
-}
-
+};
