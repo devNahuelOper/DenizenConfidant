e98815585bf513d3ae8bbb6df277ffa53e997a9b
“nahuel” <n.gorosito@yahoo.com>
2021-03-07 13:14:05 -0500

    Replace Splash beats section html with component

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ImageCarousel from "./image_carousel";
-import { Link } from "react-router-dom";
 import { fiveDaySpan } from "../../util/date_util";
 import NewsIndex from "../news/news_index";
 import NavBar from "../navbar/navbar";
 import { secretHover } from "../../util/splash_util";
 import Playlist from "./playlist";
+import FixedDjs from "./fixed_djs";
 
 class SplashPage extends React.Component {
   constructor(props) {
@@ -59,90 +59,8 @@ class SplashPage extends React.Component {
               <h1>Beats</h1>
               <h2></h2>
             </div>
-            <div className="beatwrap">
-              <ul className="beatlist">
-                <li className="song">
-                  <article>
-                    <img src={window.gesaffelsteinUrl} id="gesaffelstein" alt="Gesaffelstein"/>
-                    <Link
-                      id="gesaffelstein-link"
-                      to={`/djs/${djs["Gesaffelstein"]}`}
-                      title="Gesaffelstein"
-                    >
-                      Gesaffelstein - Depravity
-                    </Link>
-                    <audio id="depravity" src="depravity.mp3" controls></audio>
-                  </article>
-                </li>
-                <li className="song">
-                  <article>
-                    <img src={window.deadmau5Url} id="deadmau5" alt="Deadmau5"/>
-                    <Link
-                      id="deadmau5-link"
-                      to={`/djs/${djs["Deadmau5"]}`}
-                      title="Deadmau5"
-                    >
-                      Deadmau5 - S<small>peed</small>V<small>iolence</small>M
-                      <small>omentum</small>
-                    </Link>
-                    <audio id="speed" src="speed.mp3" controls></audio>
-                  </article>
-                </li>
-                <li className="song">
-                  <article>
-                    <img src={window.noisufUrl} id="noisuf" alt="Noisuf-X"/>
-                    <Link
-                      id="noisuf-link"
-                      to={`/djs/${djs["Noisuf-X"]}`}
-                      title="Noisuf-X"
-                    >
-                      Noisuf-X - Count to Seven
-                    </Link>
-                    <audio id="cseven" src="cseven.mp3" controls></audio>
-                  </article>
-                </li>
-                <li className="song">
-                  <article>
-                    <img src={window.nomanaUrl} id="nomana" alt="No Mana"/>
-                    <Link
-                      id="nomana-link"
-                      to={`/djs/${djs["No Mana"]}`}
-                      title="No Mana"
-                    >
-                      No Mana - Other Side
-                    </Link>
-                    <audio id="otherside" src="otherside.mp3" controls></audio>
-                  </article>
-                </li>
-                <li className="song">
-                  <article>
-                    <img src={window.kayzoUrl} id="kayzo" alt="Kayzo"/>
-                    <Link
-                      id="kayzo-link"
-                      to={`/djs/${djs["Kayzo"]}`}
-                      title="Kayzo"
-                    >
-                      Kayzo - The Fire
-                    </Link>
-                    <audio id="fire" src="fire.mp3" controls></audio>
-                  </article>
-                </li>
-                <li className="song">
-                  <article>
-                    <img src={window.prydzUrl} id="prydz" alt="Eric Prydz"/>
-                    <Link
-                      id="prydz-link"
-                      to={`/djs/${djs["Eric Prydz"]}`}
-                      title="Eric Prydz"
-                    >
-                      Eric Prydz - The Matrix
-                    </Link>
-                    <audio id="matrix" src="matrix.mp3" controls></audio>
-                  </article>
-                </li>
-              </ul>
-            </div>
-            <Playlist djs={randDjs}/>
+            <FixedDjs djs={djs} />
+            <Playlist djs={randDjs} />
           </section>
         </div>
       </>
