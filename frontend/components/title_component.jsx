6ddc8c8ebef47f4b0486472d2c467cf41cdc8d47
“nahuel” <n.gorosito@yahoo.com>
2023-04-13 15:54:44 -0400

    Restore focus outline on all focusable elements

--- frontend/components/title_component.jsx
+++ frontend/components/title_component.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Helmet from 'react-helmet';
+import React from "react";
+import Helmet from "react-helmet";
 
 const TitleComponent = ({ title }) => {
-  const defaultTitle = 'DC: Denizen Confidant - electronic music online';
+  const defaultTitle = "DC: Denizen Confidant - electronic music online";
   return (
     <Helmet>
-      <title>{title ? title : defaultTitle}</title>
+      <title>{title || defaultTitle}</title>
     </Helmet>
   );
 };
